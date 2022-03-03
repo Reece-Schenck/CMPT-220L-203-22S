dde0d4ee236e0e430f5daca5924de53354ba310e
Reece-Schenck <89793687+Reece-Schenck@users.noreply.github.com>
2022-03-02 22:49:23 -0500

    finished lab 6

--- JavaLabs/Lab 6/src/base/Main.java
+++ JavaLabs/Lab 6/src/base/Main.java
@@ -1,5 +1,10 @@
 package base;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Main {
     /**
      * # Lab Six
@@ -23,10 +28,59 @@ public class Main {
      */
     public static void main(String[] args) {
         // Create an Array, Populate it, and Print it out
+        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("Printing array:");
+        for (int number: numbers) {
+            System.out.println(number);
+        }
+
         // Create an ArrayList, Populate it, and Print it out
+        ArrayList<Integer> numbers2 = new ArrayList<Integer>();
+        numbers2.add(50);
+        numbers2.add(100);
+
+        System.out.println("Printing arraylist:");
+        for (int number: numbers2) {
+            System.out.println(number);
+        }
+
         // Create an LinkedList, Populate it, and Print it out
+        System.out.println("Printing linkedlist:");
+        LinkedList<String> bagel = new LinkedList<String>();
+
+        bagel.add("Top Bagel Half");
+        bagel.add("Cream Cheese");
+        bagel.add("Bottom Bagel Half");
+
+        System.out.println(bagel);
+
         // Create an Queue, Populate it, and Print it out
+        System.out.println("Printing queue:");
+        Queue<String> nextInLine = new LinkedList<String>();
+        nextInLine.add("Billy");
+        nextInLine.add("Bob");
+        nextInLine.add("Joe");
+        nextInLine.add("Jenkins");
+
+        System.out.println(nextInLine);
+
         // Create an Stack, Populate it, and Print it out
+        System.out.println("Printing stack:");
+        Stack<String> tower = new Stack<String>();
+
+        tower.push("|");
+        tower.push("||");
+        tower.push("|||");
+        tower.push("||||");
+        tower.push("|||||");
+
+        System.out.println(tower);
+        
         // Using the information in the Lab 6 Jupyter Notebook create an instance of plant here.
+        Plant someFlower = new Plant("'Cool Lookin' Flower'","The flower family?","It needs water");
+        System.out.println(someFlower.name);
+        System.out.println(someFlower.plantFamily);
+        System.out.println(someFlower.needsWatering);
     }
 }
